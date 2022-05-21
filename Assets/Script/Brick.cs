c3bd47529f1a6812d031fcaafbad0087a5f06a53
Esma12 <esra.ey1062@gmail.com>
2022-05-21 22:09:11 +0300

    cave-2 sahnesinde platform düzenlendi.
    Cavee-2 sahnesindeki player sorunu çözüldü.  Hareketli platform eklendi.

--- Assets/Script/Brick.cs
+++ Assets/Script/Brick.cs
@@ -4,23 +4,34 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-
+	Vector3 startPos;
 	Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start()
 	{
+		startPos = transform.position;
 		rb = GetComponent<Rigidbody2D>();
 	}
 
 	void OnCollisionEnter2D(Collision2D col)
 	{
-		if (col.gameObject.name.Equals("Cat"))
+		if (col.gameObject.name.Equals("Player"))
 		{
 			Invoke("DropPlatform", 0.5f);
-			Destroy(gameObject, 2f);
+			//Destroy(gameObject, 2f);
 		}
 	}
+	void OnCollisionExit2D(Collision2D col)
+	{
+		if (col.gameObject.name.Equals("Player"))
+		{
+			transform.position = startPos;
+			rb.isKinematic = true;
+			//Destroy(gameObject, 2f);
+		}
+	}
+
 
 	void DropPlatform()
 	{
