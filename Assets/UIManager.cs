34bd4fe2a4aa8f92774cb662ab0d4dca10c89112
Büşra Tatar <BusraTatar@users.noreply.github.com>
2022-05-24 02:11:03 +0300

    Npc Dialog

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-   
+    public TextMeshProUGUI dialog;
+    public string[] sentences = new string[5];
     void Start()
     {
-        
+        string [] sentences= { "I guess I don't know you. Who are you too?", "This is a very wrong question. What matters is not who I am. I've come to warn you against prophecy." };
     }
 
    
@@ -15,4 +18,15 @@ public class UIManager : MonoBehaviour
     {
         
     }
+
+    public void Next()
+    {
+        for (int i = 0; i <5; i++)
+        {   
+            string text = sentences[i];
+            dialog.text = text.ToString();
+        }
+    }
+
+
 }
