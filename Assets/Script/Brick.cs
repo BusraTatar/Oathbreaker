7fd7c8241ae4c6be0c74c65e3ad3584a671a6a7c
Esma12 <esra.ey1062@gmail.com>
2022-06-04 01:33:31 +0300

    cave sahnesindeki platform sorunları düzeltildi.
    cave sahnesindeki platform sorunları düzeltildi.

--- Assets/Script/Brick.cs
+++ Assets/Script/Brick.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-	Vector3 startPos;
+
 	Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start()
 	{
-		startPos = transform.position;
+
 		rb = GetComponent<Rigidbody2D>();
 	}
 
@@ -19,22 +19,12 @@ public class Brick : MonoBehaviour
 		if (col.gameObject.name.Equals("Player"))
 		{
 			Invoke("DropPlatform", 0.5f);
-			//Destroy(gameObject, 2f);
+			Destroy(gameObject, 2f);
 		}
 	}
-	void OnCollisionExit2D(Collision2D col)
-	{
-		if (col.gameObject.name.Equals("Player"))
-		{
-			transform.position = startPos;
-			rb.isKinematic = true;
-			//Destroy(gameObject, 2f);
-		}
-	}
-
-
 	void DropPlatform()
 	{
 		rb.isKinematic = false;
+
 	}
 }
